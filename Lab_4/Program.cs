b741234bdbdfa1fc807b3f579c76cd732cccce63
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-03-27 01:07:55 +0300

    Added some styling for output

--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -9,18 +9,18 @@ double[,] systemMatrix =
 
 double[] freeMembersColumn = { -3, 1, 0 };
 
+Console.WriteLine(new string('-', 30) + "Iteration Method" + new string('-', 30));
+
 var iteration = new IterationMethod(systemMatrix, freeMembersColumn);
 
-foreach (var item in iteration.GetResult())
-{
-    Console.WriteLine(item);
-}
+double[] iterationresult = iteration.GetResult();
 
-Console.WriteLine();
+Console.WriteLine("\nResult: X = {0}, Y = {1}, Z = {2}\n", iterationresult[0], iterationresult[1], iterationresult[2]);
+
+Console.WriteLine(new string('-', 30) + "Seidel Method" + new string('-', 30));
 
 var seidel = new IterationMethod(systemMatrix, freeMembersColumn);
 
-foreach (var item in seidel.GetResult())
-{
-    Console.WriteLine(item);
-}
+double[] seidelresult = seidel.GetResult();
+
+Console.WriteLine("\nResult: X = {0}, Y = {1}, Z = {2}\n", seidelresult[0], seidelresult[1], seidelresult[2]);
