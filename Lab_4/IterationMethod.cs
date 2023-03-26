a51ddae9d3286a1cd5317614d0a4a742989d5ac2
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-03-27 01:00:32 +0300

    Implemented Iteration method

---
+++ Lab_4/IterationMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class IterationMethod
+    {
+        public double[,] SystemMatrix { get; set; }
+        public double[] FreeMembersColumn { get; set; }
+        public IterationMethod(double[,] systemMatrix, double[] freeMembersColumn)
+        {
+            SystemMatrix = systemMatrix;
+            FreeMembersColumn = freeMembersColumn;
+        }
+        public double[] GetResult()
+        {
+            double[] result = { 0, 0, 0 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                double[] result_new = new double[3];
+
+                for (int j = 0; j < 3; j++)
+                {
+                    double s = 0;
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (j != k)
+                        {
+                            s += SystemMatrix[j, k] * result[k];
+                        }
+                    }
+
+                    result_new[j] = (FreeMembersColumn[j] - s) / SystemMatrix[j, j];
+                }
+
+                result = result_new;
+            }
+
+            return result;
+        }
+    }
+}
--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab_4;
+
+double[,] systemMatrix =
+{
+    { 2, -1, -1 },
+    { 3, 5, -1 }, 
+    { 1, -4, 10 }
+};
+
+double[] freeMembersColumn = { -3, 1, 0 };
+
+var iteration = new IterationMethod(systemMatrix, freeMembersColumn);
+
+foreach (var item in iteration.GetResult())
+{
+    Console.WriteLine(item);
+}
